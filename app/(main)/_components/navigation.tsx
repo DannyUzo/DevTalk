f1e31ff75ff1058cac320a1a20805ec1a6aebb96
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-01-08 10:13:07 +0100

    Updates: mediaQuery for the nav

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import { useCallback } from 'react';
+import { useCallback, useEffect } from "react";
 import { Logo } from "@/app/(marketing)/_components/logo";
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, FolderClock, Home, PlusSquare } from "lucide-react";
+import { useMediaQuery } from "usehooks-ts";
+import { ChevronsLeft, FolderClock, Home, PenLine } from "lucide-react";
 import Link from "next/link";
 interface Props {
   collapsePanel: () => void;
 }
 export const Navigation = ({ collapsePanel }: Props) => {
+  const isMobile = useMediaQuery("(max-width: 868px)");
+
+  useEffect(() => {
+    if (isMobile) {
+      collapsePanel();
+    }
+  }, [isMobile]);
+
   const handleClick = useCallback(() => {
     collapsePanel();
   }, [collapsePanel]);
@@ -26,21 +35,45 @@ export const Navigation = ({ collapsePanel }: Props) => {
           <ChevronsLeft className="h-6 w-6" aria-label="Collapse panel" />
         </div>
         <div className="flex h-full items-center justify-center p-6">
-          <Logo/>
+          <Logo />
         </div>
       </div>
       <div className="flex flex-col w-full items-center gap-y-5 py-3">
-        <Link href="/dashboard" passHref className={cn("flex rounded-lg border text-xl items-center gap-2 w-40 text-slate-800 px-5 py-2")}>
-          {/* <Home className="h-5 w-5"/> */}
-          Home
+        <Link
+          href="/dashboard"
+          passHref
+          className={cn(
+            "flex rounded-lg border items-center gap-2 w-50 px-5 py-2"
+          )}
+        >
+          <Home className="h-5 w-5" />
+          <span className={cn("text-lg lg:text-xl text-slate-800", isMobile && "hidden")}>
+            Home
+          </span>
         </Link>
-        <Link href="/createpost" passHref className={cn("flex rounded-lg border text-xl items-center gap-2 w-40 text-slate-800 px-5 py-2")}>
-          {/* <PlusSquare className="h-5 w-5"/> */}
-          CreatePost
+        <Link
+          href="/createpost"
+          passHref
+          className={cn(
+            "flex rounded-lg border items-center gap-2 w-50  px-5 py-2"
+          )}
+        >
+          <PenLine className="h-5 w-5" />
+          <span className={cn("text-lg md:text-xl text-slate-800", isMobile && "hidden")}>
+            CreatePost
+          </span>
         </Link>
-        <Link href="/history" passHref className={cn("flex rounded-lg border text-xl items-center gap-2 w-40 text-slate-800 px-5 py-2")}>
-          {/* <FolderClock className="h-5 w-5"/> */}
-          History
+        <Link
+          href="/history"
+          passHref
+          className={cn(
+            "flex rounded-lg border text-xl items-center gap-2 w-50 text-slate-800 px-5 py-2"
+          )}
+        >
+          <FolderClock className="h-5 w-5" />
+          <span className={cn("text-lg md:text-xl text-slate-800", isMobile && "hidden")}>
+            History
+          </span>
         </Link>
       </div>
     </aside>
