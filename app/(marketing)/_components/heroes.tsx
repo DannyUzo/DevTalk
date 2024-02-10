ec53870811f636dd1c623f6c76c3716dc50b3ab0
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-02-10 15:16:17 +0100

    Lanfing page updates

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -12,8 +12,10 @@ export const Heroes = () => {
   }
 
   return (
-    <div className="w-[90%] sm:max-w-3xl space-y-4">
-        <h1 className="text-lg"><span className="font-bold text-5xl">DevTalk</span> is a blog platform where developers and programmers can share Tech related articles, posts and content.</h1>
+    <div className="w-[90%] sm:max-w-3xl space-y-4 flex flex-col items-center">
+          <span className="font-bold text-7xl font-sans">DevTalk</span> 
+        <h1 className="text-md text-center max-w-60">
+          A blog platform where developers and programmers can share Tech related articles, posts and content.</h1>
         <div>
           <div className="flex gap-5 items-center">
             <SignUpButton name="Get Started"/>
