5c69d19d080fc5fc1b8c44caa4ee69bbb3703cd6
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-02-14 03:19:19 +0100

    updates

--- app/(main)/_components/mode-toggle-button.tsx
+++ app/(main)/_components/mode-toggle-button.tsx
@@ -22,15 +22,13 @@ export const ModeToggleButton: React.FC = () => {
         />
         <div
           className={`w-8 icon icon--moon ${
-            isChecked ? "rotate-360 scale-0" : "rotate-0 scale-100"
-          } transition-transform delay-300`}
+            isChecked ? "animate-spin-slow" : " "
+          }`}
         >
           {/* ... your moon SVG here */}
           <svg
-            className={`w-8 icon icon--moon ${
-              isChecked ? "rotate-360 scale-0" : "rotate-0 scale-100"
-            } transition-transform delay-300`}
-            xmlns="http://www.w3.org/2000/svg"
+            // className="animate-spin-slow"
+            // xmlns="http://www.w3.org/2000/svg"
             width="30"
             height="30"
             viewBox="0 0 512 512"
@@ -131,26 +129,51 @@ export const ModeToggleButton: React.FC = () => {
         </div>
         <div
           className={`w-8 icon icon--sun ${
-            isChecked ? "rotate-360 scale-100" : "rotate-0 scale-0"
-          } transition-transform delay-300`}
+            isChecked ? "animate-spin-slow" : " "
+          }`}
         >
           {/* ... your sun SVG here */}
           <svg
+            width="30"
+            height="30"
+            viewBox="0 0 64 64"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 47.5 47.5"
-            id="sun"
+            // xmlns:xlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            className="iconify iconify--emojione "
+            preserveAspectRatio="xMidYMid meet"
+            fill="#000000"
+            transform="rotate(90)"
           >
-            <defs>
-              <clipPath id="a">
-                <path d="M0 38h38V0H0v38Z"></path>
-              </clipPath>
-            </defs>
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
             <g
-              fill="#ffac33"
-              clip-path="url(#a)"
-              transform="matrix(1.25 0 0 -1.25 0 47.5)"
-            >
-              <path d="M17 35s0 2 2 2 2-2 2-2v-2s0-2-2-2-2 2-2 2v2zM35 21s2 0 2-2-2-2-2-2h-2s-2 0-2 2 2 2 2 2h2zM5 21s2 0 2-2-2-2-2-2H3s-2 0-2 2 2 2 2 2h2zM10.121 29.706s1.414-1.414 0-2.828-2.828 0-2.828 0l-1.415 1.414s-1.414 1.414 0 2.829c1.415 1.414 2.829 0 2.829 0l1.414-1.415ZM31.121 8.707s1.414-1.414 0-2.828-2.828 0-2.828 0l-1.414 1.414s-1.414 1.414 0 2.828 2.828 0 2.828 0l1.414-1.414ZM30.708 26.879s-1.414-1.414-2.828 0 0 2.828 0 2.828l1.414 1.414s1.414 1.414 2.828 0 0-2.828 0-2.828l-1.414-1.414ZM9.708 5.879s-1.414-1.414-2.828 0 0 2.828 0 2.828l1.414 1.414s1.414 1.414 2.828 0 0-2.828 0-2.828L9.708 5.879ZM17 5s0 2 2 2 2-2 2-2V3s0-2-2-2-2 2-2 2v2zM29 19c0 5.523-4.478 10-10 10-5.523 0-10-4.477-10-10 0-5.522 4.477-10 10-10 5.522 0 10 4.478 10 10"></path>
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <g fill="#ffe62e">
+                <path d="M20.5 59.7l7-7.2c-2.5-.5-4.8-1.5-6.9-2.9l-.1 10.1"></path>
+                <path d="M43.5 4.3l-7 7.2c2.5.5 4.8 1.5 6.9 2.9l.1-10.1"></path>
+                <path d="M4.3 43.5l10.1-.1C13 41.3 12 39 11.5 36.5l-7.2 7"></path>
+                <path d="M59.7 20.5l-10.1.1c1.3 2.1 2.3 4.4 2.9 6.9l7.2-7"></path>
+                <path d="M4.3 20.5l7.2 7c.5-2.5 1.5-4.8 2.9-6.9l-10.1-.1"></path>
+                <path d="M59.7 43.5l-7.2-7c-.5 2.5-1.5 4.8-2.9 6.9l10.1.1"></path>
+                <path d="M20.5 4.3l.1 10.1c2.1-1.3 4.4-2.3 6.9-2.9l-7-7.2"></path>
+                <path d="M43.5 59.7l-.1-10.1C41.3 51 39 52 36.5 52.5l7 7.2"></path>
+              </g>
+              <g fill="#ffce31">
+                <path d="M14.8 44l-4 9.3l9.3-4C18 47.8 16.2 46 14.8 44"></path>
+                <path d="M49.2 20l4-9.3l-9.2 4c2 1.5 3.8 3.3 5.2 5.3"></path>
+                <path d="M11.4 28.3L2 32l9.4 3.7c-.3-1.2-.4-2.4-.4-3.7s.1-2.5.4-3.7"></path>
+                <path d="M52.6 35.7L62 32l-9.4-3.7c.2 1.2.4 2.5.4 3.7c0 1.3-.1 2.5-.4 3.7"></path>
+                <path d="M20 14.8l-9.3-4l4 9.3c1.5-2.1 3.3-3.9 5.3-5.3"></path>
+                <path d="M44 49.2l9.3 4l-4-9.3C47.8 46 46 47.8 44 49.2"></path>
+                <path d="M35.7 11.4L32 2l-3.7 9.4c1.2-.2 2.5-.4 3.7-.4s2.5.1 3.7.4"></path>
+                <path d="M28.3 52.6L32 62l3.7-9.4c-1.2.3-2.4.4-3.7.4s-2.5-.1-3.7-.4"></path>
+                <circle cx="32" cy="32" r="19"></circle>
+              </g>
             </g>
           </svg>
         </div>
