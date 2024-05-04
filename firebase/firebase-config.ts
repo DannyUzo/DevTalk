7ef001be047b760a3e5e0dc96d0f1a2285050e3a
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-05-04 13:48:58 +0100

    Updates:

--- firebase/firebase-config.ts
+++ firebase/firebase-config.ts
@@ -1,10 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBkVW_j2DH23QJ9NyuezJ9NLislwCHSGFI",
   authDomain: "devtalk-d4ab2.firebaseapp.com",
@@ -20,3 +17,5 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
