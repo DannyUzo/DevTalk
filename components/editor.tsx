24fcca748fc19adf0381ecd4d0dc463d1eeb17b3
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-05-04 13:47:44 +0100

    Updates:  Editor

---
+++ components/editor.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+import { useTheme } from "next-themes";
+
+interface EditorProps {
+    onChange: (value: string) => void,
+    initialContent?: string;
+    editable?: boolean;
+}
+ const Editor = ({onChange, initialContent, editable}: EditorProps) => {
+    const { resolvedTheme } = useTheme()
+
+    // const editor = useBlockNote();
+
+    const editor: BlockNoteEditor = useBlockNote({
+        editable,
+        initialContent: initialContent ? initialContent as PartialBlock[] : undefined,
+        onEditorContentChange: (editor) => {
+            onChange(editor.topLevelBlocks, null, 2)
+        }
+    })
+
+    return (
+        <div>
+            <BlockNoteView editor={editor} theme={resolvedTheme === "dark" ? "dark" : "light"} />
+        </div>
+    )
+}
+export default Editor
