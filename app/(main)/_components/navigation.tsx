930f48d1769bcb7ddb31d08ef69bc1d3dd78999f
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-05-08 22:11:15 +0100

    updates

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -25,13 +25,13 @@ export const Navigation = ({ collapsePanel }: Props) => {
   }, [collapsePanel]);
 
   return (
-    <aside className="group/sidebar h-[100vh] overflow-y-auto flex  justify-between flex-col z-[99999]">
+    <aside className="group/sidebar overflow-y-auto flex h-full justify-between flex-col z-[99999]">
       <div>
         <div
           onClick={handleClick}
           role="button"
           className={cn(
-            "w-6 h-6 text-muted-foreground rounded-sm hover:bg-neutral-100 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+            "w-6 h-6 text-muted-foreground rounded-sm hover:bg-neutral-100 dark:hover:bg-neutral-600 absolute top-3 left-2 opacity-0 group-hover/sidebar:opacity-100 transition"
           )}
         >
           <ChevronsLeft className="h-6 w-6" aria-label="Collapse panel" />
@@ -40,7 +40,7 @@ export const Navigation = ({ collapsePanel }: Props) => {
           <Logo />
         </div>
       </div>
-      <div className="flex flex-col w-full items-center gap-y-5 py-3 px-5">
+      <div className="flex flex-col w-full items-center gap-y-5 py-3 ">
         <Link
           href="/dashboard"
           passHref
