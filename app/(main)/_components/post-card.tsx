a00829df3b49124a7030b9bfe183a36cec95df6a
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-05-28 17:56:33 +0100

    Post card created

---
+++ app/(main)/_components/post-card.tsx
@@ -0,0 +1,29 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+
+interface CardProps {
+    Title: string;
+    Author: string;
+    AuthorImg: string;
+
+}
+
+export const PostCard = ({ Title, Author, AuthorImg }: CardProps) => {
+    return (
+        <Card className="w-70 sm:w-[480px] flex flex-col justify-between">
+            <CardHeader>
+                <div className="flex">
+                <Avatar>
+                    <AvatarImage src={AuthorImg} alt="display picture"/>
+                    <AvatarFallback>DT</AvatarFallback>
+                </Avatar>
+                <CardDescription>{Author}</CardDescription>
+                </div>
+                <CardTitle>{Title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+
+            </CardContent>
+        </Card>
+    )
+}
